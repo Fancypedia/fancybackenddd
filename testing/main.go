598bb2537870248f815e931daae8af16b2ebebb4
raulmahya <raulmahya11@gmail.com>
2024-01-06 19:14:39 +0700

    push

--- testing/main.go
+++ testing/main.go
@@ -8,24 +8,79 @@ import (
 	peda "github.com/Fancypedia/fancybackenddd"
 )
 
-func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+func Withing(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodOptions {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	fmt.Fprintf(w, peda.GCFUpdateFE("0d6171e848ee9efe0eca37a10813d12ecc9930d6f9b11d7ea594cac48648f022", "MONGOULBI", "proyek3", "dosen", "frontend", r))
+	fmt.Fprintf(w, peda.GCFPostGeoIntersects("mongoenv", "petapedia", r))
 }
 
-func handlerRequests() {
-	http.HandleFunc("/", HelloHTTP)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func Nearr(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, peda.PostNear("mongoenv", "petapediaaa", r))
+}
+
+func Polygonnnfix(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, peda.Polygonnn("mongoenv", "petapediaaa", r))
+}
+
+func NearSpeheree(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, peda.NearSpeheree("mongoenv", "petapediaaa", r))
+}
+
+func Box(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, peda.BoxFix("mongoenv", "petapediaaa", r))
 }
 
 func main() {
 	handlerRequests()
+}
+func handlerRequests() {
+	http.HandleFunc("/intersect", Withing)
+	http.HandleFunc("/near", Nearr)
+	http.HandleFunc("/polygon", Polygonnnfix)
+	http.HandleFunc("/nearsphere", NearSpeheree)
+	http.HandleFunc("/box", Box)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
