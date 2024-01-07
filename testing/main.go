07f6e78d1bccc5b99217730cc1ea58a704f36479
raulmahya <raulmahya11@gmail.com>
2024-01-07 19:57:41 +0700

    puhs

--- testing/main.go
+++ testing/main.go
@@ -73,6 +73,44 @@ func Box(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, peda.BoxFix("mongoenv", "petapediaaa", r))
 }
 
+func Radius(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, peda.CenterSphere("mongoenv", "petapediaaa", r))
+}
+func Max(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, peda.MaxDistance("mongoenv", "petapediaaa", r))
+}
+
+func Mix(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, peda.MinDistanceee("mongoenv", "petapediaaa", r))
+}
+
 func main() {
 	handlerRequests()
 }
@@ -82,5 +120,8 @@ func handlerRequests() {
 	http.HandleFunc("/polygon", Polygonnnfix)
 	http.HandleFunc("/nearsphere", NearSpeheree)
 	http.HandleFunc("/box", Box)
+	http.HandleFunc("/radius", Radius)
+	http.HandleFunc("/max", Max)
+	http.HandleFunc("/min", Mix)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
